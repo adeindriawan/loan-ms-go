2bd6d33a4696c44ca0df981a0635cec6936f258b
Ade Indriawan <ade.indriawan@amarbank.co.id>
2024-01-05 18:25:16 +0700

    moved db queries to respository

---
+++ internal/repository/user_repository.go
@@ -0,0 +1,64 @@
+package repository
+
+import (
+	"database/sql"
+	"loan-ms-go/internal/entity"
+)
+
+func AddUser(db *sql.DB, user entity.User) (entity.User, error) {
+	query := "INSERT INTO users (name, email) VALUES (?, ?)"
+	result, err := db.Exec(query, user.Name, user.Email)
+
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	lastInsertedID, err := result.LastInsertId()
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	user.ID = int(lastInsertedID)
+	return user, nil
+}
+
+func GetUsers(db *sql.DB) ([]entity.User, error) {
+	rows, err := db.Query("SELECT id, name, email FROM users")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []entity.User
+	for rows.Next() {
+		var user entity.User
+		err := rows.Scan(&user.ID, &user.Name, &user.Email)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func GetUserByID(db *sql.DB, userID int) (entity.User, error) {
+	query := "SELECT * FROM users WHERE id = ?"
+	row := db.QueryRow(query, userID)
+
+	var user entity.User
+	err := row.Scan(&user.ID, &user.Name, &user.Email)
+	if err != nil {
+		return entity.User{}, err
+	}
+	return user, nil
+}
+
+func UpdateUser(db *sql.DB, user entity.User) (sql.Result, error) {
+	stmt := "UPDATE users SET name = ?, email = ? WHERE id = ?"
+	return db.Exec(stmt, user.Name, user.Email, user.ID)
+}
--- internal/usecase/user_usecase.go
+++ internal/usecase/user_usecase.go
@@ -3,43 +3,21 @@ package usecase
 import (
 	"database/sql"
 	"loan-ms-go/internal/entity"
+	"loan-ms-go/internal/repository"
 )
 
 func AddUser(db *sql.DB, user entity.User) (entity.User, error) {
-	query := "INSERT INTO users (name, email) VALUES (?, ?)"
-	result, err := db.Exec(query, user.Name, user.Email)
-
+	addedUser, err := repository.AddUser(db, user)
 	if err != nil {
 		return entity.User{}, err
 	}
 
-	lastInsertedID, err := result.LastInsertId()
-	if err != nil {
-		return entity.User{}, err
-	}
-
-	user.ID = int(lastInsertedID)
-	return user, nil
+	return addedUser, nil
 }
 
 func GetUsers(db *sql.DB) ([]entity.User, error) {
-	rows, err := db.Query("SELECT id, name, email FROM users")
+	users, err := repository.GetUsers(db)
 	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	var users []entity.User
-	for rows.Next() {
-		var user entity.User
-		err := rows.Scan(&user.ID, &user.Name, &user.Email)
-		if err != nil {
-			return nil, err
-		}
-		users = append(users, user)
-	}
-
-	if err := rows.Err(); err != nil {
 		return nil, err
 	}
 
@@ -47,11 +25,7 @@ func GetUsers(db *sql.DB) ([]entity.User, error) {
 }
 
 func GetUser(db *sql.DB, userID int) (entity.User, error) {
-	query := "SELECT * FROM users WHERE id = ?"
-	row := db.QueryRow(query, userID)
-
-	var user entity.User
-	err := row.Scan(&user.ID, &user.Name, &user.Email)
+	user, err := repository.GetUserByID(db, userID)
 	if err != nil {
 		return entity.User{}, err
 	}
@@ -59,6 +33,9 @@ func GetUser(db *sql.DB, userID int) (entity.User, error) {
 }
 
 func UpdateUser(db *sql.DB, user entity.User) (sql.Result, error) {
-	stmt := "UPDATE users SET name = ?, email = ? WHERE id = ?"
-	return db.Exec(stmt, user.Name, user.Email, user.ID)
+	result, err := repository.UpdateUser(db, user)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
